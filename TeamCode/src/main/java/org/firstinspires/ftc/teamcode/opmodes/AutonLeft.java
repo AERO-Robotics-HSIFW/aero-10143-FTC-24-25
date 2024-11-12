fa1c688823129257dfe4887e5577ecb7be84471e
ritvikniraula <saniritvik@gmail.com>
2024-11-12 17:12:26 -0600

    Outline of Auton, the idea is have 2 autons for left side and right side, Leo and I will be working on auton.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonLeft.java
@@ -0,0 +1,12 @@
+// This file is the code for when the robot is on the LEFT side of the field
+
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.hardware.Onbot_HardwareITD;
+import org.firstinspires.ftc.teamcode.statemachines.StateMachines;
+
+@Autonomous(name="AutonLeft", group= "Autonomous")
+public class AutonLeft {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonRight.java
@@ -0,0 +1,12 @@
+// This file is the code for when the robot is on the RIGHT side of the field
+
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.hardware.Onbot_HardwareITD;
+import org.firstinspires.ftc.teamcode.statemachines.StateMachines;
+
+@Autonomous(name="AutonRight", group= "Autonomous")
+public class AutonRight {
+
+}
